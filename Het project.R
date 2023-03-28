a84989aa8c0a37c8180b282ebaac988fa9f083fe
louis <Louis@vandenbruwaene.eu>
2023-03-28 16:05:04 +0200

    Alle gegevens ingelezen en gemanipuleerd

--- Het project.R
+++ Het project.R
@@ -1,9 +1,15 @@
+# 1) Gegevens inlezen en manipuleren 
 airbnb <- read.csv2("~/2e Bachelor/stat404/stat_project_2023/airbnb.csv", sep="", stringsAsFactors=TRUE)
-View(airbnb)
-airbnb$attr = (9*airbnb$attr)/(airbnb$attr + 1) + 1
-View(airbnb)
-hist(airbnb$attr)
 range = max(airbnb$attr)-min(airbnb$attr)
-range 
-airbnb$attr = 1 + 9*(airbnb$attr - min(airbnb$attr) /range)
-airbnb$attr
+range2 = max(airbnb$rest)-min(airbnb$rest)
+airbnb$attr = 1 + 9*((airbnb$attr - min(airbnb$attr)) /range)
+airbnb$rest = 1 + 9*((airbnb$rest - min(airbnb$rest)) /range2)
+airbnb$lat = NULL
+airbnb$lng = NULL
+airbnb$room = factor(airbnb$room, levels = c(1,2,3), labels = c("volledige woning","afzonderlijke kamer", "gedeelde kamer"))
+airbnb$host = factor(airbnb$host, levels = c(0,1,2), labels = c(" enige beschikbare woning", "2 tot 4 beschikbare woningen", "meer dan 4 beschikbare woningen"))
+attach(airbnb)
+
+# 2) Gegevens verkennnen
+summary(airbnb)
+hist(log(realSum)
