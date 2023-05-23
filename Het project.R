69d77206d5e6c814a250ea1dc622a0f64e711d67
louis <Louis@vandenbruwaene.eu>
2023-05-23 19:29:33 +0200

    wat verder gewerkt aan verslag(lineaire regressie)

--- Het project.R
+++ Het project.R
@@ -13,7 +13,8 @@ airbnb$lat  = NULL
 airbnb$lng  = NULL
 airbnb$room = factor(airbnb$room, levels = c(1,2,3), labels = c("volledige woning","afzonderlijke kamer", "gedeelde kamer"))
 airbnb$host = factor(airbnb$host, levels = c(0,1,2), labels = c("enige beschikbare woning", "2 tot 4 beschikbare woningen", "meer dan 4 beschikbare woningen"))
-
+install.packages("moments")
+library(moments)
 clear_envir()  # Verwijdert onnodige variabelen
 attach(airbnb)
 
@@ -41,8 +42,6 @@ table(room) # zeer weinig gedeelde kamers
 table(room)/length(room)
 barplot(table(room))
 barplot(table(room)/length(room))
-plot(room,log10(realSum))
-plot(room,(realSum))
 
 # 3: capacity
 # Kwantitatief, discreet (positief)
@@ -52,8 +51,7 @@ hist(capacity)
 table(capacity)/length(capacity)
 barplot(table(capacity))
 barplot(table(capacity)/length(capacity))
-plot(capacity,realSum)
-plot(capacity,log10(realSum))
+
 # 4: bedrooms
 # Kwantitatief, discreet (positief)
 summary(bedrooms)
@@ -61,8 +59,7 @@ table(bedrooms)
 table(bedrooms)/length(bedrooms)
 barplot(table(bedrooms))
 barplot(table(bedrooms)/length(bedrooms))
-plot(bedrooms,realSum)
-plot(bedrooms, log10(realSum))
+
 # 5: dist
 # Kwantitatief, continu (positief)
 hist(dist) # duidelijk rechtsscheef 
@@ -71,8 +68,7 @@ summary(dist)  # er zijn niet meteen uitschieters, behalve na de logtransf is er
 range(dist)
 range(dist)[2]-range(dist)[1]
 cov(realSum, dist)  # sterk omgekeerd evenredig verband; (kleine afstand = hoge prijs)
-plot(dist,realSum)
-plot(dist, log10(realSum))
+
 # 6: metro
 # Kwantitatief, continu (positief)
 hist(metro) # rechtsscheef met meeste gegevens links (kleine afstand)
@@ -81,8 +77,7 @@ summary(metro)
 range(metro)     
 range(metro)[2]-range(metro)[1] # minder spreiding dan dist
 cov(realSum, metro) # een kleinere afstand zal een hogere prijs met zich meetrekken, maar minder erg dan bij dist
-plot(metro,realSum)
-plot(metro, log10(realSum))
+
 # 7: attr
 # Kwantitatief, continu (tussen 1 en 10)
 hist(attr) # zeer rechtsscheef
@@ -91,9 +86,6 @@ summary(attr)
 range(attr)
 range(attr)[2]- range(attr)[1] # de attr ligt per constructie tussen 1 en 10
 cov(realSum, attr) # hoe hoger de attr score, hoe hoger de prijs
-plot(attr,realSum)
-plot(attr, log10(realSum))
-plot(log10(attr), log10(realSum))
 
 # 8: rest
 # Kwantitatief, continu (tussen 1 en 10)
@@ -103,8 +95,7 @@ summary(rest)
 range(rest)
 range(rest)[2]- range(rest)[1] # de rest ligt per constructie tussen 1 en 10
 cov(realSum, rest) # er is duidelijk een positieve samenhang tussen de prijs en de restaurantscore
-plot(rest,realSum)
-plot(rest, log10(realSum))
+
 # 9: host
 # Kwalitatief, ordinaal
 # Meeste verblijven hebben een gastheer die maar één woning ter beschikking heeft
@@ -112,8 +103,7 @@ y = table(host)
 x = barplot(table(host), main = "Aantal beschikbare woningen van de gastheer\nbij een woning", names.arg = c("1", "2-4", ">4"), col = c("snow2", "snow3", "snow4"), ylim = c(0,max(table(host))*1.15), space = 0)
 text(x, table(host)+30, labels = as.character(y))
 pie(table(host), main = "Aantal beschikbare woningen van de gastheer\nbij een woning", labels = c("1", "2-4", ">4"))
-plot(host,realSum)
-plot(host, log10(realSum))
+
 # 10: cleanliness
 # Kwalitatief, ordinaal (tussen 1 en 10)
 # REDEN: modale score van 1-10, maar een 9 is niet per-se "properder" dan een 8,
@@ -123,8 +113,7 @@ plot(host, log10(realSum))
 # Exponentieel verdeeld?
 table(cleanliness)
 hist(cleanliness, col = c("snow2", "snow3"))
-plot(cleanliness,realSum)
-plot(cleanliness, log10(realSum))
+
 # 11: satistfaction
 # Kwantitatief, continu (tussen 1 en 10)
 # REDEN: terwijl cleanliness kwalitatief zou zijn, lijkt satisfaction mij toch
@@ -135,8 +124,6 @@ plot(cleanliness, log10(realSum))
 hist(satisfaction, breaks = 1:10, col = c("snow2", "snow3"))
 hist(airbnb[satisfaction>=7,11], breaks = seq(7, 10, 0.2), col = c("snow2", "snow3"), main = "Histogram van de satisfaction scores tussen 7 en 10")
 table(floor(satisfaction))
-plot(satisfaction,realSum)
-plot(satisfaction, log10(realSum))
 sd(realSum, capacity, bedrooms, dist, metro, attr, rest, cleanliness)
 sd(realSum)
 sd(capacity)
@@ -251,7 +238,6 @@ as.numeric(diff(result_3$estimate))  # Verschil tussen gemiddelden
 ###normaliteit nagaan
 
 shapiro.test(realSum)
-qqnorm(realSum); qqline(realSum)
 #realSum is duidelijk niet normaal verdeeld == > Spearman
 cor.test(realSum, dist, method = c("spearman"), exact = FALSE) #waarschijnlijk afhankelijk
 cor.test(realSum, metro, method = c("spearman"), exact = FALSE) #waarschijnlijk afhankelijk
@@ -310,6 +296,7 @@ par(mfrow = c(2,2))
 plot(simpelmodel1)
 par(mfrow = c(1,1))
 summary(simpelmodel1)
+skewness(attr)
 
 simpelmodel2 = lm(log10(realSum)~log10(attr)); simpelmodel2
 par(mfrow = c(2,2))
@@ -346,12 +333,12 @@ lines(sort(x_i2), predictie2[order(x_i2) ,3] , col = "green")
 par(mfrow = c(1,1))
 
 
-model = lm(realSum~satisfaction+rest+attr+metro+dist)
-summary(model)
-model = update(model ,.~. -rest )
-summary(model)
-model = update(model ,.~. -metro )
-summary(model)#Dit is een basismodel, maar zeer slecht
+naiefmodel = lm(realSum~satisfaction+rest+attr+metro+dist)
+summary(naiefmodel)
+model = update(naiefmodel ,.~. -rest )
+summary(naiefmodel)
+model = update(naiefmodel ,.~. -metro )
+summary(naiefmodel)#Dit is een basismodel, maar zeer slecht
 
 par(mfrow = c(2,2))
 plot(model)
@@ -407,6 +394,4 @@ onsmodel = update(onsmodel, .~.*dommy)
 summary(onsmodel)
 onsmodel = update(onsmodel, .~.-satisfaction:dommy)
 summary(onsmodel)#dit model is goed
-par(mfrow = c(2,2))
-plot(onsmodel)
-par(mfrow = c(1,1))
+
